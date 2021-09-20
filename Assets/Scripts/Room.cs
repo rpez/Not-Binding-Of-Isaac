fee09fb02b2d99b5a4f1544b05e8fc6aff9d5917
Reetu Kontio <reetu.kontio@gmail.com>
2021-09-20 11:05:22 +0300

    add initial floor randomizer

---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public (int, int) m_coordinates;
+
+    public Room[] m_neigbours = new Room[4];
+    public int m_neighbourAmount = 0;
+
+    public Room((int, int) coordinates)
+    {
+        m_coordinates = coordinates;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AttachRoom(int direction, Room room)
+    {
+        m_neigbours[direction] = room;
+        m_neighbourAmount++;
+    }
+}
---
+++ Assets/Scripts/RoomManager.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomManager : MonoBehaviour
+{
+    public int m_roomAmount;
+    public GameObject m_roomPrefab;
+
+    private List<Room> m_rooms = new List<Room>();
+    private Dictionary<(int, int), Room> m_coordsToRoom = new Dictionary<(int, int), Room>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateNewFloor(5);
+        foreach (Room room in m_rooms)
+        {
+            GameObject.Instantiate(m_roomPrefab, new Vector3(room.m_coordinates.Item1 * 15f, room.m_coordinates.Item2 * 10f, 0), Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GenerateNewFloor(int rooms)
+    {
+        List<(int, int)> grid = new List<(int, int)>();
+
+        grid.Add((0, 0));
+        int createdRooms = 1;
+
+        m_rooms.Add(new Room((0, 0)));
+        m_coordsToRoom.Add((0, 0), m_rooms[0]);
+        while (createdRooms < rooms)
+        {
+            int rIndex = Random.Range(0, grid.Count);
+            (int, int) coords = grid[rIndex];
+            Room selected = m_coordsToRoom[coords];
+            if (selected.m_neighbourAmount >= 4) continue;
+
+            int rDir = Random.Range(0, 4);
+            if (selected.m_neigbours[rDir] != null) continue;
+            (int, int) newCoords = coords;
+            switch (rDir)
+            {
+                case 0:
+                    newCoords = (coords.Item1, coords.Item2 + 1);
+                    break;
+                case 1:
+                    newCoords = (coords.Item1 - 1, coords.Item2);
+                    break;
+                case 2:
+                    newCoords = (coords.Item1, coords.Item2 - 1);
+                    break;
+                case 3:
+                    newCoords = (coords.Item1 + 1, coords.Item2);
+                    break;
+                default:
+                    Debug.LogError("Wrong direction while creating rooms.");
+                    break;
+            }
+            if (grid.Contains(newCoords)) continue;
+
+            Room newRoom = new Room(newCoords);
+            selected.AttachRoom(rDir, newRoom);
+            m_rooms.Add(newRoom);
+            m_coordsToRoom.Add(newCoords, newRoom);
+            grid.Add(newCoords);
+
+            createdRooms++;
+        }
+    }
+}

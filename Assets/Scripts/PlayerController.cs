1d3e448f6f886cfe2b0cf29c088f09785e7b68f4
luupanu <22541529+luupanu@users.noreply.github.com>
2021-09-22 03:57:41 +0300

    added collision and fine-tuned movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,36 +4,31 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float m_speed = 1;
     public GameObject m_projectile;
+    public Rigidbody2D m_rigidBody;
+
+    public Vector2 m_movementInput;
+    // public Vector2 m_shootingInput; // TO-DO: Make shooting velocity-based
+
+    // Player movement values
+    public float m_linearDragMoving = 16.0f;
+    public float m_angularDragMoving = 6.0f;
+    public float m_linearDragStopping = 800.0f;
+    public float m_angularDragStopping = 800.0f;
+
+    public float m_velocityScalar = 8.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float delta = Time.deltaTime;
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.Translate(Vector3.up * delta * m_speed);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(Vector3.left * delta * m_speed);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(Vector3.down * delta * m_speed);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(Vector3.right * delta * m_speed);
-        }
+        ProcessInputs();
+        Move();
 
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
@@ -51,6 +46,51 @@ public class PlayerController : MonoBehaviour
         {
             Shoot(Vector3.right);
         }
+    }
+
+    void ProcessInputs() {
+        m_movementInput = new Vector2(Input.GetAxis("MoveHorizontal"), Input.GetAxis("MoveVertical"));
+
+        m_movementInput.Normalize();
+
+        // TO-DO: Add a small bit of acceleration at the beginning to Isaac here.
+
+        if (MovementKeysHeld()) {
+            m_rigidBody.drag = m_linearDragMoving;
+            m_rigidBody.angularDrag = m_angularDragMoving;
+        } else {
+            m_rigidBody.drag = m_linearDragStopping;
+            m_rigidBody.angularDrag = m_angularDragStopping;
+        }
+    }
+
+    private bool MovementKeysHeld()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D)) {
+            return true;
+        }
+        return false;
+    }
+
+    private bool MovementKeysUp()
+    {
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D)) {
+            return true;
+        }
+        return false;
+    }
+
+    private bool MovementKeysDown()
+    {
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D)) {
+            return true;
+        }
+        return false;
+    }
+
+    private void Move()
+    {
+        m_rigidBody.velocity = m_movementInput * m_velocityScalar;
     }
 
     private void Shoot(Vector3 dir)

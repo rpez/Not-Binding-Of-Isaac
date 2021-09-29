28bf38cb57e7848828787ca24fdf33e7a9c001bf
Reetu Kontio <reetu.kontio@gmail.com>
2021-09-29 18:09:51 +0300

    fix (almost) all shooting problems

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -15,14 +15,22 @@ public class Projectile : MonoBehaviour
     public void Init(Vector2 direction, Vector2 parentVelocity)
     {
         m_rigidBody = GetComponent<Rigidbody2D>();
+
         Vector2 additional = Vector2.zero;
-        if (Vector2.Dot(parentVelocity, direction) * parentVelocity.magnitude >= m_parentSpeedThreshold)
+        if (Mathf.Abs(parentVelocity.x) >= m_parentSpeedThreshold
+            && (Mathf.Sign(direction.x) == Mathf.Sign(parentVelocity.x)
+            || direction.x < 0.1f))
         {
-            additional = parentVelocity;
-            additional = additional.normalized + direction.normalized;
-            additional.Normalize();
+            additional = new Vector2(Mathf.Sign(parentVelocity.x), additional.y);
         }
-        
+        if (Mathf.Abs(parentVelocity.y) >= m_parentSpeedThreshold
+            && (Mathf.Sign(direction.y) == Mathf.Sign(parentVelocity.y)
+            || direction.x < 0.1f))
+        {
+            additional = new Vector2(additional.x, Mathf.Sign(parentVelocity.y));
+        }
+        additional.Normalize();
+
         m_rigidBody.velocity = (direction + additional * m_parentSpeedScale) * m_speed;
     }
 

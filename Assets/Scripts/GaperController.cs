3dbf5d6dc4d349507ed6544800dd7fc9cc420b62
luupanu <22541529+luupanu@users.noreply.github.com>
2021-10-07 21:17:07 +0300

    make gapers try to avoid each other in order to swarm the player

--- Assets/Scripts/GaperController.cs
+++ Assets/Scripts/GaperController.cs
@@ -34,19 +34,74 @@ public class GaperController : MonoBehaviour, MonsterController
 
         Vector3 playerPos = m_player.GetComponent<Collider2D>().bounds.center - transform.position;
 
-        // for debugging
-        // Debug.DrawRay(transform.position, toPlayer * 15f, Color.green);
-
         // 15f length for the ray should be enough for current room size
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, playerPos, 15f, LayerMask.GetMask("Player", "Wall"), 0, 0);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, playerPos, 15f);
 
-        bool canSeePlayer = hit.collider != null && hit.collider.gameObject.tag == "Player";
+        bool debugRays = true;
 
-        if (canSeePlayer)
+        if (hit.collider.gameObject.tag == "Wall")
         {
-            MoveTo(playerPos);
-        } else {
             // AStarMoveTowardsPlayer();
+        }
+        else if (hit.collider.gameObject.tag == "Player")
+        {
+            if (debugRays)
+            {
+                Vector3 drawRay1 = playerPos;
+                drawRay1.Normalize();
+                Debug.DrawRay(transform.position, drawRay1, Color.green);
+            }
+            
+            MoveTo(playerPos);
+        }
+        else if (hit.collider.gameObject.tag == "Enemy")
+        {
+            // Try to dodge your fellow enemy. This way the enemies can try to surround the player.
+
+            // Get the bounding box of the blocking enemy as 4x Vector3
+            Bounds b = hit.collider.bounds;
+            Vector3[] bounds = new Vector3[]
+            {
+                new Vector3(b.min.x, b.min.y),
+                new Vector3(b.min.x, b.max.y),
+                new Vector3(b.max.x, b.min.y),
+                new Vector3(b.max.x, b.max.y)
+            };
+
+            // Find the second closest bounding box corner to me
+            Vector3 closest = Vector3.positiveInfinity;
+            Vector3 secondClosest = Vector3.positiveInfinity;
+
+            foreach (Vector3 v in bounds)
+            {
+                if (Vector3.Distance(v, transform.position) < closest.magnitude)
+                {
+                    secondClosest = closest;
+                    closest = v;
+                }
+                else if (Vector3.Distance(v, transform.position) < secondClosest.magnitude)
+                {
+                    secondClosest = v;
+                }
+            }
+
+            // Reflect the vector secondClosest across the line playerPos, where playerPos is a straight line towards the player.
+            // This makes the enemies try to scatter a bit in order to not block each other.
+            // See: https://en.wikipedia.org/wiki/Reflection_(mathematics)#Reflection_across_a_line_in_the_plane
+            Vector3 enemyPos = secondClosest - transform.position;
+            Vector3 reflection = 2 * Vector3.Dot(enemyPos, playerPos) / Vector3.Dot(playerPos, playerPos) * playerPos - enemyPos;
+
+            MoveTo(reflection);
+
+            if (debugRays)
+            {
+                Vector3 drawRay2 = enemyPos;
+                drawRay2.Normalize();
+                Vector3 drawRay3 = reflection;
+                drawRay3.Normalize();
+                Debug.DrawRay(transform.position, drawRay2, Color.blue);
+                Debug.DrawRay(transform.position, drawRay3, new Color(1.0f, 0.0f, 1.0f));
+            }
         }
     }
 

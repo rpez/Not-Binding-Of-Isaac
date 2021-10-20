3084cd33d8c102126737e02157670035f83ed47f
Reetu Kontio <reetu.kontio@gmail.com>
2021-10-20 23:19:18 +0300

    fix namespace error

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -1,11 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Animations;
 using UnityEngine;
 
 public class Item : MonoBehaviour
 {
-    public AnimatorController m_mutationAnimator;
+    public RuntimeAnimatorController m_mutationAnimator;
 
     public float m_damageModifier = 1f;
     public float m_speedModifier = 1f;
